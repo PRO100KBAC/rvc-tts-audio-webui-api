61ebce56958d02f4e84ddafaa425d4c6bee16903
Nikita <pro100kbac33@gmail.com>
2023-11-11 02:05:46 +0500

    added two different nvidia torch versions

--- install.bat
+++ install.bat
@@ -3,8 +3,9 @@ py -m venv venv
 call venv\scripts\activate
 pip install -r requirements.txt --no-cache-dir
 echo "Select device"
-echo "1. Nvidia GPU"
-echo "2. CPU"
+echo "1. Nvidia GPU cu118"
+echo "2. Nvidia GPU cu121"
+echo "3. CPU"
 Set /p device=""
 if "%device%"=="1" (
 pip uninstall -y torch torchvision torchaudio
@@ -15,6 +16,14 @@ pip install -r requirementsapi.txt --no-cache-dir
 start run.bat
 )
 if "%device%"=="2" (
+pip uninstall -y torch torchvision torchaudio
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --no-cache-dir
+py -m venv venvapi
+call venvapi\scripts\activate
+pip install -r requirementsapi.txt --no-cache-dir
+start run.bat
+)
+if "%device%"=="3" (
 py -m venv venvapi
 call venvapi\scripts\activate
 pip install -r requirementsapi.txt --no-cache-dir
--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt --no-cache-dir
 echo "Select device"
-echo "1. Nvidia GPU"
-echo "2. AMD ROCM"
-echo "3. CPU"
+echo "1. Nvidia GPU cu118"
+echo "2. Nvidia GPU cu121"
+echo "3. AMD ROCM"
+echo "4. CPU"
 read -p "Select device: " device
 if [[ $device == "1" ]]; then
   pip uninstall -y torch torchvision torchaudio
@@ -18,13 +19,21 @@ if [[ $device == "1" ]]; then
 fi
 if [[ $device == "2" ]]; then
   pip uninstall -y torch torchvision torchaudio
-  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6 --no-cache-dir
+  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --no-cache-dir
   python -m venv venvapi
   source venvapi/bin/activate
   pip install -r requirementsapi.txt --no-cache-dir
   ./run.sh
 fi
 if [[ $device == "3" ]]; then
+  pip uninstall -y torch torchvision torchaudio
+  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6 --no-cache-dir
+  python -m venv venvapi
+  source venvapi/bin/activate
+  pip install -r requirementsapi.txt --no-cache-dir
+  ./run.sh
+fi
+if [[ $device == "4" ]]; then
   python -m venv venvapi
   source venvapi/bin/activate
   pip install -r requirementsapi.txt --no-cache-dir
