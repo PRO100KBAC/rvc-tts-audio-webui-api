9a9db0fc2c277d7efd12cdfe68e4d58cb0f8ef5e
Nikita <pro100kbac33@gmail.com>
2023-11-11 02:35:45 +0500

    changed installers for both OS

--- install.bat
+++ install.bat
@@ -3,9 +3,8 @@ py -m venv venv
 call venv\scripts\activate
 pip install -r requirements.txt --no-cache-dir
 echo "Select device"
-echo "1. Nvidia GPU cu118"
-echo "2. Nvidia GPU cu121"
-echo "3. CPU"
+echo "1. Nvidia GPU"
+echo "2. CPU"
 Set /p device=""
 if "%device%"=="1" (
 pip uninstall -y torch torchvision torchaudio
@@ -16,14 +15,6 @@ pip install -r requirementsapi.txt --no-cache-dir
 start run.bat
 )
 if "%device%"=="2" (
-pip uninstall -y torch torchvision torchaudio
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --no-cache-dir
-py -m venv venvapi
-call venvapi\scripts\activate
-pip install -r requirementsapi.txt --no-cache-dir
-start run.bat
-)
-if "%device%"=="3" (
 py -m venv venvapi
 call venvapi\scripts\activate
 pip install -r requirementsapi.txt --no-cache-dir
--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt --no-cache-dir
 echo "Select device"
-echo "1. Nvidia GPU cu118"
-echo "2. Nvidia GPU cu121"
+echo "1. Nvidia GPU"
 echo "3. AMD ROCM"
 echo "4. CPU"
 read -p "Select device: " device
@@ -18,22 +17,6 @@ if [[ $device == "1" ]]; then
   ./run.sh
 fi
 if [[ $device == "2" ]]; then
-  pip uninstall -y torch torchvision torchaudio
-  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 --no-cache-dir
-  python -m venv venvapi
-  source venvapi/bin/activate
-  pip install -r requirementsapi.txt --no-cache-dir
-  ./run.sh
-fi
-if [[ $device == "3" ]]; then
-  pip uninstall -y torch torchvision torchaudio
-  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6 --no-cache-dir
-  python -m venv venvapi
-  source venvapi/bin/activate
-  pip install -r requirementsapi.txt --no-cache-dir
-  ./run.sh
-fi
-if [[ $device == "4" ]]; then
   python -m venv venvapi
   source venvapi/bin/activate
   pip install -r requirementsapi.txt --no-cache-dir
